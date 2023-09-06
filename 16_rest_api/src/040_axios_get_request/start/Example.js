971101084244e80fbc368293620ad9b65955fad4
mori-y-ww <mori.y@world-wing.com>
2023-09-06 18:40:16 +0900

    train

--- 16_rest_api/src/040_axios_get_request/start/Example.js
+++ 16_rest_api/src/040_axios_get_request/start/Example.js
@@ -1,5 +1,13 @@
+import { useEffect } from "react";
+import axios from 'axios';
+
+
 const Example = () => {
-  return <></>;
+  useEffect(()=>{
+    axios.get('http://localhost:3003/user').then((res)=>{
+      console.log(res.data)
+    });
+  })
 };
 
 export default Example;
